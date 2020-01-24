04a99ec4a38b371f9382a9b76bd457a93df9beb0
andersonfrfilho <andersonfrfilho@gmail.com>
2020-01-23 23:18:20 -0300

    adding files for github pasges

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [filterMonsters, setFilterMonsters] = useState(monsters);
+  const [searchField, setSearchField] = useState('');
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        setMonsters(users);
+        setFilterMonsters(users);
+      });
+  }, []);//eslint-disable-line
+  useEffect(() => {
+    const filterMonstersAux = !searchField
+      ? monsters
+      : monsters.filter(monster =>
+          monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+        );
+    setFilterMonsters(filterMonstersAux);
+  }, [searchField]);//eslint-disable-line
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monster Rolodex</h1>
+      <SearchBox
+        placeholder="search monster"
+        handleChange={text => {
+          setSearchField(text.target.value);
+        }}
+      />
+      <CardList monsters={filterMonsters} />
     </div>
   );
 }
