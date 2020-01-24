6af391a3ecf0472bb6b473685778a63a801ba3ef
andersonfrfilho <andersonfrfilho@gmail.com>
2020-01-23 23:17:46 -0300

    Updates

---
+++ precache-manifest.a1d0390e98c1e55d4e8f050078b1eb5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea12ac3b9c0f8b59a2b58862af33cfa0",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "9bc69915a443529160fd",
+    "url": "/monster-rolodex/static/css/main.76253d13.chunk.css"
+  },
+  {
+    "revision": "617e0f92aa616ae4c4f4",
+    "url": "/monster-rolodex/static/js/2.6fd2d76f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.6fd2d76f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bc69915a443529160fd",
+    "url": "/monster-rolodex/static/js/main.fa117888.chunk.js"
+  },
+  {
+    "revision": "23fb47eb6f86565db1b0",
+    "url": "/monster-rolodex/static/js/runtime-main.47b0ddc3.js"
+  }
+]);
